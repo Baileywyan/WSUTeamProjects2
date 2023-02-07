98142ca5cbfc5a3e903aa1fbd129026e1b466b3e
Samuel <59849928+SJeffries19@users.noreply.github.com>
2023-02-07 15:31:28 -0500

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,28 @@
+package edu.wright.teamproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Calendar cal = Calendar.getInstance();
+
+        String currentDate = DateFormat.getDateInstance().format(cal.getTime());
+
+        TextView textViewDate = findViewById(R.id.textView);
+        textViewDate.setText(currentDate);
+
+        TextView textViewDate2 = findViewById(R.id.textView7);
+        textViewDate2.setText("Last Updated: " + currentDate);
+    }
+}
