701fa4453440bafb3af3703afecee6657ce2af1a
Samuel <59849928+SJeffries19@users.noreply.github.com>
2023-03-01 11:54:26 -0500

    Add files via upload

--- src/MainActivity.java
+++ src/MainActivity.java
@@ -8,7 +8,8 @@ import android.widget.TextView;
 import android.widget.Button;
 import android.content.Intent;
 
-import java.text.DateFormat;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
 import java.util.*;
 import java.io.*;
 
@@ -18,40 +19,114 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Calendar cal = Calendar.getInstance();
+        final double tempNum = 80;
+        final double pHNum = 6;
 
-        String currentDate = DateFormat.getDateInstance().format(cal.getTime());
+        SimpleDateFormat df = new SimpleDateFormat("MMM dd, yyyy KK:mm aaa");
+        final String currentDate = df.format(cal.getTime());
 
         TextView textViewDate = findViewById(R.id.textView);
         textViewDate.setText(currentDate);
 
-        TextView viewUpdate = findViewById(R.id.textView7);
 
-        try{
-            InputStream inputStream = getAssets().open("UpdateLog.txt");
-            int size = inputStream.available();
-            byte[] b = new byte[size];
-            inputStream.read(b);
-            String str = new String(b);
-            TextView textViewDate2 = findViewById(R.id.textView7);
-            textViewDate2.setText("Last Updated: " + str);
-        }
-        catch(IOException e){
-            viewUpdate.setText("Last Updated: NA");
-        }
+        final TextView temp = findViewById(R.id.textView5);
+        temp.setText(tempNum + "\u00B0F");
 
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        final TextView pH = findViewById(R.id.textView6);
+        pH.setText(String.valueOf(pHNum));
+
+
+        final TextView viewUpdate = findViewById(R.id.textView7);
+
+        readFile(viewUpdate);
+
+        Button connect = findViewById(R.id.button);
+        connect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openActivity2();
             }
         });
+
+        Button scan = findViewById(R.id.button2);
+        scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    File path = getApplicationContext().getFilesDir();
+                    FileOutputStream outputStream = new FileOutputStream(new File(path, "UpdateLog.txt"));
+                    outputStream.write(currentDate.getBytes());
+                    outputStream.close();
+
+                    readFile(viewUpdate);
+                }
+                catch(IOException e){
+                    viewUpdate.setText("Failure to update");
+                }
+
+                scanTank(tempNum, pHNum, temp, pH);
+
+            }
+        });
+
     }
 
-    public void openActivity2(){
+    public void openActivity2()
+    {
         Intent intent = new Intent(this, Activity2.class);
         startActivity(intent);
+    }
+
+    void readFile(TextView textView)
+    {
+        File path = getApplicationContext().getFilesDir();
+        File readFrom = new File(path, "UpdateLog.txt");
+        byte[] content = new byte[(int)readFrom.length()];
+
+        try{
+            FileInputStream inputStream = new FileInputStream(readFrom);
+            inputStream.read(content);
+            String str = new String(content);
+            TextView textViewDate2 = findViewById(R.id.textView7);
+            textViewDate2.setText("Last Updated: \n" + str);
+        }
+        catch(IOException e){
+            textView.setText("Last Updated: NA");
+        }
+    }
+
+    void scanTank(double tempNum, double pHNum, TextView temp, TextView pH)
+    {
+        DecimalFormat df1 = new DecimalFormat("0.0");
+        DecimalFormat df2 = new DecimalFormat("0.00");
+
+        Random rand = new Random();
+
+        double num = rand.nextDouble();
+        int pM = rand.nextInt(3);
+
+
+        if(pM == 1){
+            tempNum = tempNum - num;
+            temp.setText(df1.format(tempNum) + "\u00B0F");
+
+            pHNum = pHNum - (num * .1);
+            pH.setText(df2.format(pHNum));
+        }
+        else{
+            tempNum = tempNum + num;
+            temp.setText(df1.format(tempNum) + "\u00B0F");
+
+            pHNum = pHNum + (num * .1);
+            pH.setText(df2.format(pHNum));
+        }
+
+        if(pHNum > 6.1 || tempNum > 81)
+        {
+            System.out.println("AHHHH");
+        }
+
+
     }
 }
